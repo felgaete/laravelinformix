a3f80cf18cd6ba78a505ffebe225910dfeb0cd3e
fhenne <fhenne@users.noreply.github.com>
2016-03-08 12:00:49 +0100

    Update Connection.php

--- src/fhenne/Informix/Connection.php
+++ src/fhenne/Informix/Connection.php
@@ -33,7 +33,7 @@ class Connection extends \Illuminate\Database\Connection
         $this->connection = $connector->createConnection($dsn, $config, $options);
 
         // Select database
-        $this->db = $this->connection->selectDatabase($config['database']);
+        // $this->db = $this->connection->selectDatabase($config['database']);
 
         // $this->useDefaultPostProcessor();
     }
