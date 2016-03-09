c03d7a4178c870b50a681ded3b0d21e4e1018ed2
fhenne <fhenne@users.noreply.github.com>
2016-03-09 12:15:23 +0100

    Update Processor.php

--- src/fhenne/Informix/Query/Processor.php
+++ src/fhenne/Informix/Query/Processor.php
@@ -5,8 +5,8 @@ use fhenne\Informix\Query\Builder;
 
 class Processor extends BaseProcessor
 {
-	public function processColumnListing($results)
-    {
-        return array_values(array_map(function($r) { $r = (object) $r; return $r->strtoupper(column_name); }, $results));
-    }
+	// public function processColumnListing($results)
+ //   {
+ //       return array_values(array_map(function($r) { $r = (object) $r; return $r->strtoupper(column_name); }, $results));
+ //   }
 }
